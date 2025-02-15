9c0d3f678e065202e1b12dbfa7beb1ee7ecec0ac
Hanguyen2505 <hanguyentkl2505@gmail.com>
2025-02-15 23:06:05 +0700

    add comment entity

--- app/src/main/java/com/example/socialmediaapp/data/entity/Comment.kt
+++ app/src/main/java/com/example/socialmediaapp/data/entity/Comment.kt
@@ -1,6 +1,29 @@
 package com.example.socialmediaapp.data.entity
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "Comment",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["userId"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Post::class,
+            parentColumns = ["postId"],
+            childColumns = ["postId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+
+)
 data class Comment(
+    @PrimaryKey(autoGenerate = false)
     val commentId: String = "",
     val userId: String = "",
     val postId: String = "",
--- app/src/main/java/com/example/socialmediaapp/data/room/database/AppDatabase.kt
+++ app/src/main/java/com/example/socialmediaapp/data/room/database/AppDatabase.kt
@@ -6,9 +6,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.socialmediaapp.data.entity.Comment
 import com.example.socialmediaapp.data.entity.Follower
 import com.example.socialmediaapp.data.entity.Post
 import com.example.socialmediaapp.data.entity.User
+import com.example.socialmediaapp.data.room.Comment.CommentDao
 import com.example.socialmediaapp.data.room.follower.FollowerDao
 import com.example.socialmediaapp.data.room.post.PostDao
 import com.example.socialmediaapp.data.room.user.UserDao
@@ -18,12 +20,13 @@ import com.example.socialmediaapp.other.MediaTypeConverter
     entities = [
         Post::class,
         User::class,
-        Follower::class
+        Follower::class,
+        Comment::class
     ],
-    version = 2,
+    version = 4,
     exportSchema = true,
     autoMigrations = [
-        AutoMigration(from = 1, to = 2)
+        AutoMigration(from = 3, to = 4)
     ]
 )
 @TypeConverters(MediaTypeConverter::class) // Register the converter
@@ -31,6 +34,7 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun postDao(): PostDao
     abstract fun userDao(): UserDao
     abstract fun followerDao(): FollowerDao
+    abstract fun commentDao(): CommentDao
 
     companion object {
         @Volatile
